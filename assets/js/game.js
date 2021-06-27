02520105623a6eb536058fdea323cebf664564a8
David Crane <ddcrane2@gmail.com>
2021-06-27 15:34:43 -0500

    fixed loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+  // place fight function code block here . . .
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -59,8 +60,12 @@ var fight = function(enemyName) {
     }
   }
 }
+}
 
 for(var i = 0; i< enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  // call fight function with enemy-robot
   fight(enemyNames[i]);
 }
 
